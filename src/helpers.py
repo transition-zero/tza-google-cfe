1432cd1febb83c8baa974059508a6a519f9e15e7
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2024-11-26 12:13:06 +0000

    add doc string to helpers

--- src/helpers.py
+++ src/helpers.py
@@ -3,11 +3,40 @@ import yaml
 
 
 def setup_dir(path_to_dir):
+    """
+    Creates a directory if it does not already exist.
+
+    Parameters:
+    path_to_dir (str): The path to the directory to be created.
+
+    Returns:
+    None
+    """
     if not os.path.exists(path_to_dir):
         os.makedirs(path_to_dir)
 
 
 def load_configs(path):
-    with open(path, 'r') as file:
-        configs = yaml.safe_load(file)
+    """
+    Load configuration settings from a YAML file.
+
+    Args:
+        path (str): The file path to the YAML configuration file.
+
+    Returns:
+        dict: A dictionary containing the configuration settings.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        yaml.YAMLError: If there is an error parsing the YAML file.
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"The file at {path} does not exist.")
+    
+    try:
+        with open(path, 'r') as file:
+            configs = yaml.safe_load(file)
+    except yaml.YAMLError as e:
+        raise yaml.YAMLError(f"Error parsing YAML file: {e}")
+    
     return configs
