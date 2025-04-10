abb3da29c2ac7520770d571487e37b03e7ca0a62
djwels <dwelsby@TZ-LPT-011>
2025-04-10 21:15:31 +0100

    Tested Irfan's capacity factor code to ensure correct generator in each bus is being picked up

--- src/cfe.py
+++ src/cfe.py
@@ -165,7 +165,9 @@ def PrepareNetworkForCFE(
                 )
 
                 # get capacity factors if technology is renewable
-                generator_names = network.generators[network.generators.type == technology].index
+                generator_names = network.generators.index[
+                    (network.generators["type"] == technology) & (network.generators["bus"] == bus)
+                ]
                 cf = network.generators_t.p_max_pu[generator_names]
                 if cf.empty:
                     cf = params['p_max_pu']
