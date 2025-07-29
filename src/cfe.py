8237c8a0877f072117704a24f1219ad4136618bc
djwels <dan.w@transitionzero.org>
2025-07-29 18:31:17 +0000

    Hash out p_nom_max in cfe.py

--- src/cfe.py
+++ src/cfe.py
@@ -198,7 +198,7 @@ def PrepareNetworkForCFE(
                             # unique technology parameters by bus
                             p_nom = 0, # starting capacity (MW)
                             p_nom_min = 0, # minimum capacity (MW)
-                            p_nom_max = params['p_nom_max'],
+                            #p_nom_max = params['p_nom_max'],
                             p_max_pu = cf, # capacity factor
                             p_min_pu = params['p_min_pu'], # minimum capacity factor
                             efficiency = params['efficiency'], # efficiency
@@ -236,7 +236,7 @@ def PrepareNetworkForCFE(
                             # unique technology parameters by bus
                             p_nom = 0, # starting capacity (MW)
                             p_nom_min = 0, # minimum capacity (MW)
-                            p_nom_max = params['p_nom_max'],
+                            #p_nom_max = params['p_nom_max'],
                             p_max_pu = cf, # capacity factor
                             p_min_pu = params['p_min_pu'], # minimum capacity factor
                             efficiency = params['efficiency'], # efficiency
